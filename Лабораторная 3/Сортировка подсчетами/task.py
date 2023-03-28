f07b416b755de3fa49eddec61a46a370c3c16a6b
yanab <yanaenot0910@gmail.cpm>
2023-03-28 19:37:29 +0300

    Задание 2

--- Лабораторная 3/Сортировка подсчетами/task.py
+++ Лабораторная 3/Сортировка подсчетами/task.py
@@ -1,6 +1,5 @@
 from typing import Sequence
 
-
 def sort(container: Sequence[int]) -> Sequence[int]:
     """
     Сортировка подсчетами
@@ -12,4 +11,15 @@ def sort(container: Sequence[int]) -> Sequence[int]:
     :param container: Массив, который надо отсортировать
     :return: Отсортированный в порядке возрастания массив
     """
-    ...  # TODO реализовать алгоритм сортировки подсчетами
+    if not container:
+        return []
+    max_val = max(container)
+    counts = [0] * (max_val+1)
+    for x in container:
+        counts[x] += 1
+    res = []
+    for i in range(max_val+1):
+        res.extend([i] * counts[i])
+    return res
+
+
