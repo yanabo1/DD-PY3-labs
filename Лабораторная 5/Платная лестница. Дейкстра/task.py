81909733c68acd7618a4ed05d154e0dac79b5798
yanab <yanaenot0910@gmail.cpm>
2023-04-18 23:53:52 +0300

    Задания лабораторных 4 и 5

--- Лабораторная 5/Платная лестница. Дейкстра/task.py
+++ Лабораторная 5/Платная лестница. Дейкстра/task.py
@@ -3,18 +3,25 @@ from typing import Union
 import networkx as nx
 
 
-def stairway_path(graph: nx.DiGraph) -> Union[float, int]:
-    """
-    Рассчитайте минимальную стоимость подъема на верхнюю ступень,
-    если мальчик умеет наступать на следующую ступень и перешагивать через одну.
+def stairway_graph(stairway):
+    n = len(stairway)
+    G = nx.DiGraph()
+    for i in range(n):
+        G.add_node(i, weight=stairway[i])
+    for i in range(n - 2):
+        G.add_edge(i, i + 2, weight=0)
+        G.add_edge(i, i + 3, weight=0)
+    G.add_edge(0, 1, weight=0)
+    G.add_edge(0, 2, weight=0)
+    return G
 
-    :param graph: Взвешенный направленный граф NetworkX, по которому надо рассчитать стоимости кратчайших путей
-    :return: минимальная стоимость подъема на верхнюю ступень
-    """
-    ...  # TODO c помощью функции из модуля networkx найти стоимость кратчайшего пути до последней лестницы
+
+def stairway_path(graph: nx.DiGraph) -> Union[float, int]:
+    path = nx.dijkstra_path(graph, 0, graph.number_of_nodes() - 1)
+    return sum(graph[u][v]["weight"] for u, v in zip(path[:-1], path[1:]))
 
 
 if __name__ == '__main__':
     stairway = (5, 11, 43, 2, 23, 43, 22, 12, 6, 8)
-    stairway_graph = ...  # TODO записать взвешенный граф, а лучше написать функцию, которая формирует граф по стоимости ступеней
+    stairway_graph = stairway_graph(stairway)
     print(stairway_path(stairway_graph))  # 72
